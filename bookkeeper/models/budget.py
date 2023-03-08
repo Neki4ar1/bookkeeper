dde01a7798afb08052ef4bfd6926a8b2ed2e9913
Neki4ar <Neki4ar@gmail.com>
2023-03-08 15:14:08 +0300

    changed budget.py

--- bookkeeper/models/budget.py
+++ bookkeeper/models/budget.py
@@ -13,20 +13,18 @@ class Budget:
     Бюджет
     limit - limit on period
     spent - how much money have been spent in period
-    period - day, week, or month
     pk - primary key
     """
-    limit: float
+    limit_on: float
     spent: float
-    period: str
+    date: str
     pk: int = 0
-
-    def __init__(self, limit, spent, period, pk):
-        self.limit = limit
-        self.spent = spent
-        self.period = period
-        self.pk = pk
+    #
+    # def __init__(self, limit, spent, pk):
+    #     self.limit = limit
+    #     self.spent = spent
+    #     self.pk = pk
 
     def set_limit_bud(self, limit: float) -> None:
-        self.limit = limit
+        self.limit_on = limit
 
