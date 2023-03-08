9b8012e0b5f88e5a25080e87feed17d8db8a98a3
Neki4ar <Neki4ar@gmail.com>
2023-03-08 19:16:00 +0300

    added bud_repo and fix #27

--- bookkeeper/main.py
+++ bookkeeper/main.py
@@ -1,26 +1,29 @@
 # -*- coding: utf8 -*-
 """
-Демонстрация TableView на основе https://www.pythonguis.com/tutorials/qtableview-modelviews-numpy-pandas/
+Демонстрация TableView
+на основе https://www.pythonguis.com/tutorials/qtableview-modelviews-numpy-pandas/
 """
+import sys
 
 from PySide6.QtWidgets import QApplication
 from bookkeeper.view.expense_view import MainWindow
 from bookkeeper.presenter.expense_presenter import ExpensePresenter
 from bookkeeper.models.category import Category
 from bookkeeper.models.expense import Expense
+from bookkeeper.models.budget import Budget
 from bookkeeper.repository.sqlite_repository import SQliteRepository
 
-import sys
 
 DB_NAME = 'test.db'
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    view = MainWindow()
-    model = None  # TODO: здесь должна быть модель
+    VIEW = MainWindow()
+    MODEL = None  # TODO: здесь должна быть модель
     cat_repo = SQliteRepository[Category](DB_NAME, Category)
     exp_repo = SQliteRepository[Expense](DB_NAME, Expense)
+    bud_repo = SQliteRepository[Budget](DB_NAME, Budget)
 
-    window = ExpensePresenter(model, view, cat_repo, exp_repo)  # TODO: передать три репозитория
+    window = ExpensePresenter(MODEL, VIEW, [cat_repo, exp_repo, bud_repo])
     window.show()
     app.exec()
