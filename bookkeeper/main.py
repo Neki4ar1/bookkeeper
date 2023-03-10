c62e649e387e6c70f1de5331a3c1d4c7019aa527
Neki4ar <Neki4ar@gmail.com>
2023-03-11 00:31:21 +0300

    returned main.py to old version

--- bookkeeper/main.py
+++ bookkeeper/main.py
@@ -7,6 +7,7 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 from bookkeeper.view.expense_view import MainWindow
+from bookkeeper.view.redactor_view import RedactorWindow
 from bookkeeper.presenter.expense_presenter import ExpensePresenter
 from bookkeeper.models.category import Category
 from bookkeeper.models.expense import Expense
