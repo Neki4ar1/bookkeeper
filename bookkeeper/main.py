186f59c7ff937cdba5b7d8cf8e2a3b09affcfc7f
Neki4ar <Neki4ar@gmail.com>
2023-03-12 18:38:53 +0300

    added realization of bud repo at start point

--- bookkeeper/main.py
+++ bookkeeper/main.py
@@ -7,7 +7,6 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 from bookkeeper.view.expense_view import MainWindow
-from bookkeeper.view.redactor_view import RedactorWindow
 from bookkeeper.presenter.expense_presenter import ExpensePresenter
 from bookkeeper.models.category import Category
 from bookkeeper.models.expense import Expense
@@ -20,10 +19,13 @@ DB_NAME = 'test.db'
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     VIEW = MainWindow()
-    MODEL = None  # TODO: здесь должна быть модель
+    MODEL = None
     cat_repo = SQliteRepository[Category](DB_NAME, Category)
     exp_repo = SQliteRepository[Expense](DB_NAME, Expense)
     bud_repo = SQliteRepository[Budget](DB_NAME, Budget)
+    bud_repo.add(Budget(limit_on=1000, spent=0))
+    bud_repo.add(Budget(limit_on=7000, spent=0))
+    bud_repo.add(Budget(limit_on=30000, spent=0))
 
     window = ExpensePresenter(MODEL, VIEW, [cat_repo, exp_repo, bud_repo])
     window.show()
